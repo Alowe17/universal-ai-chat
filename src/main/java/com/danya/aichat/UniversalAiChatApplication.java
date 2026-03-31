44bdc7da6f7de6ede12a9f1ed23cf41f6aafc1f7
alowe17 <p.danil51007@gmail.com>
2026-03-31 22:07:31 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/danya/aichat/UniversalAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.danya.aichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalAiChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danya/aichat/UniversalAiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danya.aichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalAiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
